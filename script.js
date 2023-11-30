6613bf314ce71e03c0cea42fa3b1d52305dca5e6
mlaskowski7 <mtlaskowski7@gmail.com>
2023-12-01 00:14:44 +0100

    adding js functionality for infinite scrolling

--- script.js
+++ script.js
@@ -1,19 +1,79 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+let ready = false;
+let imagesloaded = 0;
+let totalimages = 0;
+let photosArray = [];
 
 const count = 10;
 const apiKey = 'rVo5go2dLnbhHweQklTFTHLyBnzq7BKJlTetel83dmE';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function imageLoaded(){
+    imagesloaded++;
+    if(imagesloaded === totalimages){
+        ready = true;
+        console.log('ready= ', ready)
+    }
+}
+
+function setAttributes(element, attributes){
+    for(const key in attributes){
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+
+function displayPhotos(){
+    loader.hidden = true;
+    totalimages = photosArray.length;
+    console.log('total images:', totalimages);
+
+    photosArray.forEach((photo) =>{
+        const item = document.createElement('a');
+        setAttributes(item,{
+            href: photo.links.html,
+            target:'_blank',
+
+        });
+
+        const img = document.createElement('img'); 
+        setAttributes(img,{
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+
+        img.addEventListener('load',imageLoaded);
+
+        item.appendChild(img)
+        imageContainer.appendChild(item)
+    });
+}
 
 
 async function getPhotos(){
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos();
     } catch (error) {
+        loader.hidden= false;
+        const error_message = document.createElement('p');
+        error_message.textContent = "The API is limited to 50 requests an hour, the limit has been exceeded";
+        error_message.classList.add("error");
+        document.body.appendChild(error_message);
         
     }
 }
+
+
+window.addEventListener('scroll', () =>{
+    if(window.innerHeight+window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        getPhotos();
+    }
+});
 
 
 getPhotos();
